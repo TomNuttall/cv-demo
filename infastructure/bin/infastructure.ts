19a3f657d23ab7870dad19063ebd941d0b150827
Tom <tsnuttall@gmail.com>
2025-04-01 15:52:35 +0100

    feat: infastructure create lambda

--- infastructure/bin/infastructure.ts
+++ infastructure/bin/infastructure.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { FrontendStack } from '../lib/frontend-stack'
+import { BackendStack } from '../lib/backend-stack'
 import 'dotenv/config'
 
 const config = {
@@ -17,5 +18,9 @@ new FrontendStack(app, 'cv-template-frontend-stack', {
   env: config.env,
   bucketName: `${process.env.BUCKET_NAME}`,
   cloudfrontId: `${process.env.CLOUDFRONT_ID}`,
+  repoName: `${process.env.REPO_NAME}`,
+})
+new BackendStack(app, 'cv-template-backend-stack', {
+  env: config.env,
   repoName: `${process.env.REPO_NAME}`,
 })
