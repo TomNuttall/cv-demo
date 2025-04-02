092efff42888e450771ec589fbc031fd97235157
Tom <tsnuttall@gmail.com>
2025-04-02 08:38:30 +0100

    feat: Resolvers get data from s3 bucket

--- lambda/tests/resolvers/queries/getCoveringLetter.test.ts
+++ lambda/tests/resolvers/queries/getCoveringLetter.test.ts
@@ -1,13 +1,36 @@
-import { describe, it, expect } from '@jest/globals'
+import { beforeEach, describe, it, expect } from '@jest/globals'
+import { mockClient } from 'aws-sdk-client-mock'
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+import { sdkStreamMixin } from '@smithy/util-stream'
+import { Readable } from 'stream'
+
 import queries from '../../../src/resolvers/queries'
 import { Context } from '../../../src/context'
 
+const s3Mock = mockClient(S3Client)
+const context: Context = {
+  s3Client: new S3Client(),
+}
+
 describe('getCoveringLetter', () => {
+  beforeEach(() => {
+    s3Mock.reset()
+  })
+
   it('should return query result', async () => {
     // Arrange
+    const response = {
+      letter: [''],
+    }
+    const stream = new Readable()
+    stream.push(JSON.stringify(response))
+    stream.push(null)
+
+    const sdkStream = sdkStreamMixin(stream)
+    s3Mock.on(GetObjectCommand).resolves({ Body: sdkStream })
+
     const parent = {}
     const args = {}
-    const context: Context = {}
 
     // Act
     //@ts-ignore
