c1a6cc750a9c300f223eee35e4553b80d8b52740
Tom <tsnuttall@gmail.com>
2025-07-30 21:24:42 +0100

    Permissions fix

--- lambda/src/permissions/permissions.ts
+++ lambda/src/permissions/permissions.ts
@@ -1,15 +1,12 @@
 import { shield, allow, deny } from 'graphql-shield'
 import { isAuthenticated } from './rule.js'
 
-const permissions = shield(
-  {
-    Query: {
-      getMyApplications: isAuthenticated,
-      getCV: isAuthenticated,
-      getCoveringLetter: isAuthenticated,
-    },
+const permissions = shield({
+  Query: {
+    getMyApplications: isAuthenticated,
+    getCV: isAuthenticated,
+    getCoveringLetter: isAuthenticated,
   },
-  { fallbackRule: deny, fallbackError: 'Error' },
-)
+})
 
 export default permissions
